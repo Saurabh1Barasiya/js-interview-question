fcc27ba00c38be5fce076299be52e78e761f62b0
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-06-17 15:46:57 +0530

    map function in js

---
+++ Day-14/script.js
@@ -0,0 +1,22 @@
+// map function hame 1 new array return karta hai.
+// existing array ke base par.
+// map function array ke har 1 element par apply ho jata hai.
+
+// map is also a higher order function....
+
+// higher order function are thoes function wich takes function as an input
+// or return a function as an output.
+
+// so map takes a callback function as an input. it means ita a higher order function.
+
+
+
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log("Original Array",arr);
+
+const modifiedArray = arr.map((ele)=>{
+    return ele + 10;
+});
+
+console.log("Modified Array",modifiedArray);
+
