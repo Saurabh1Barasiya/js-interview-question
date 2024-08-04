631b028d0a377891372c1593fee291449214c13c
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-08-04 17:46:01 +0530

    replace function.

---
+++ Day-27/script.js
@@ -0,0 +1,16 @@
+// replace() ye new string return karta hai.
+
+let myString = "hello my name is saurabh and i am belongs to harrai chhindwara.";
+let newString = myString.replace('hello','hi');
+console.log(newString);
+
+// hi my name is saurabh and i am belongs to harrai chhindwara.
+
+// ye only first match ko hi replace karta hai and new string return karta hai.
+
+
+
+// ye javascript ES2021 ka new feature hai.  abhi proper output nahi aa raha hai.
+// let str = "kya kya kya karte ho tum";
+// let changed = str.replaceAll(/World/g,'kese'); 
+// console.log(changed);
