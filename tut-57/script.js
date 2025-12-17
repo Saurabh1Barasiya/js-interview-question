d4bd1f625121d04a7250dc506b44a433e556ce48
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-17 15:25:01 +0530

    array simple question.

---
+++ tut-57/script.js
@@ -0,0 +1,11 @@
+// console.log(arguments);
+
+
+
+// problem 
+const emptyArr = Array(10,20); 
+console.log(emptyArr);     // [ 10,20 ]
+
+
+const emptyArr1 = new Array(10,20,30);
+console.log(emptyArr1);  // [10,20,30]
