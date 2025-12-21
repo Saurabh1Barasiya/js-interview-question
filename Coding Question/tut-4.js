02117b5efbc9e8d691c014d35832bdafaa7fa57c
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-21 09:34:24 +0530

    find secound minimum element in array.

---
+++ Coding Question/tut-4.js
@@ -0,0 +1,18 @@
+// find secound minimum array.
+
+let numberArray = [10, 20, 30, 4, 2, 5, 4, 63, 1, 5];
+let minimum = Number.MAX_SAFE_INTEGER;
+let secoundMinimum = minimum;
+
+for (let element of numberArray) {
+    if (element < minimum) {
+        secoundMinimum = minimum;
+        minimum = element
+    } else if(element < secoundMinimum){
+        secoundMinimum = element;
+    }
+}
+
+console.log("Secound minimum element is :", secoundMinimum);
+
+// Secound minimum element is : 2
