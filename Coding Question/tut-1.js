02c16d0782afebc608eecea0808a17e136dfe81f
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-21 08:47:33 +0530

    abc ->  ['a', 'ab', 'abc', 'b', 'bc', 'c' ]

---
+++ Coding Question/tut-1.js
@@ -0,0 +1,17 @@
+// abc
+// ['a,'ab','abc,'b','bc','c'];
+
+function convert(newString){
+    result = [];
+    for(let i = 0;i<newString.length;i++){
+        for(j=i+1;j<=newString.length;j++){
+            result.push(newString.slice(i,j))
+        }
+    }
+    return result;
+}
+
+let mystr = "abc";
+ans = convert(mystr);
+console.log(ans);  //[ 'a', 'ab', 'abc', 'b', 'bc', 'c' ]
+console.log(ans.join(' '));  //[ 'a', 'ab', 'abc', 'b', 'bc', 'c' ]
