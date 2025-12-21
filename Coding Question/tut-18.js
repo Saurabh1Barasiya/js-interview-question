17e11c07a3c1387bf8677b6a359fd86f892e642e
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-21 14:16:36 +0530

    reverse string with some method.

---
+++ Coding Question/tut-18.js
@@ -0,0 +1,9 @@
+// reverse a string.
+
+let s = "saurabh";
+
+// console.log(s.split(''))  // [ 's', 'a', 'u', 'r', 'a', 'b', 'h']
+// console.log(s.split('').reverse())  // [ 'h', 'b', 'a', 'r', 'u', 'a','s']
+// console.log(s.split('').reverse().join(''))  // hbaruas
+
+console.log(s.split('').reverse().join('')) // hbaruas
